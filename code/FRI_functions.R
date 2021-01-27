2bdf3ad57ade9e0550c4ac0f9569533e47ebe998
Matt Wilson <77399165+wilson-matt@users.noreply.github.com>
2021-01-27 11:32:57 -0500

    Initial folder tree created

---
+++ code/FRI_functions.R
@@ -0,0 +1,12 @@
+#This code should be at the beginning of all of your scripts.
+
+#Save repo to your C Drive in exactly this format:
+#It requires that you create a folder in your C Drive called "GitHub" - make sure the G and H are capital.
+#This is what mine would look like. Yours will be the same format of last name, first initial, middle initial and all lowercase.
+home.path <- "C:/GitHub/wilsonmj"
+setwd(home.path)
+
+#Within the home.path your code, data, and plots will be stored in separate folders. 
+  #Do not change the names of folders cloned from the template. 
+  #If a file path is broken, change the actual path on your computer to match the code.
+
