4dbc84d101a8df62c0d4d0da868c6bcbe18e1ef7
Matt Wilson <77399165+wilson-matt@users.noreply.github.com>
2021-01-27 18:54:34 -0500

    added paths for subfolders

--- code/FRI_functions.R
+++ code/FRI_functions.R
@@ -4,15 +4,23 @@
 #It requires that you create a folder in your C Drive called "GitHub" - make sure the G and H are capital.
 #This is what mine would look like. Yours will be the same format of last name, first initial, middle initial and all lowercase.
 home.path <- "C:/GitHub/wilsonmj"
-setwd(home.path)
 
 #Temp - delete for students
 home.path <- "C:/GitHub/student-research-template"
 #
+data.path <- paste(home.path, "/data", sep = "")
+
+code.path <- paste(home.path, "/code", sep = "")
+
+plot.path <- paste(home.path, "/plots", sep = "")
 
 #Within the home.path your code, data, and plots will be stored in separate folders. 
   #Do not change the names of folders cloned from the template. 
   #If a file path is broken, change the actual path on your computer to match the code.
+
+
+
+
 
 #Pull data from Access Database ----
 #Load dependencies
@@ -21,12 +29,12 @@ library(ggplot2)
 library(readxl)
 
 #Set data file as work
-setwd(paste(home.path, "/data", sep = ""))
+setwd()
+?odbcConnectAccess2007
 
+db<- file.path("C:/GitHub/Sandbox/Flowpath.accdb")
 
-db<- file.path("Flowpath.accdb")
-
-channel<- odbcConnectAccess2007(db, uid="resslerd")
+channel<- odbcConnectAccess2007(db)
 
 TRAGD<-sqlFetch(channel, "TransducerRecordAGD")
 
@@ -34,3 +42,6 @@ View(TRAGD)
 
 
 
+write.csv(TRAGD, file = "this.csv")
+
+?write.csv
